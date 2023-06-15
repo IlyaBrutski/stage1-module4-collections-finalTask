1e4707b260a3985ae620155ccd26487980b1015c
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-06-15 18:49:21 +0300

    FinalTask_3

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -4,12 +4,27 @@ import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
-        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.comparing(String::length).reversed());
+        Map<Integer, Queue<String>> map = new HashMap<>();
+
         for(Map.Entry<String, Set<String>> m : projects.entrySet()){
             if(m.getValue().contains(developer)){
-                queue.add(m.getKey());
+                if(map.containsKey(m.getKey().length())) {
+                    map.get(m.getKey().length()).add(m.getKey());
+                } else {
+                    PriorityQueue<String> q = new PriorityQueue<>(Comparator.comparing(String::toString).reversed());
+                    q.add(m.getKey());
+                    map.put(m.getKey().length(), q);
+                }
             }
         }
-        return new ArrayList<>(queue);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for(Map.Entry<Integer, Queue<String>> m : map.entrySet()){
+            queue.add(m.getKey());
+        }
+        List<String> list = new ArrayList<>();
+        for(Integer i : queue){
+            list.addAll(map.get(i));
+        }
+        return list;
     }
 }
