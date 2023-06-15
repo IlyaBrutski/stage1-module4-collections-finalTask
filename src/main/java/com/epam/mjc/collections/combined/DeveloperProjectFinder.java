1f502d7e93ee4e44dffd54ad83c220485da7de56
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-06-15 17:18:39 +0300

    FinalTask

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.comparing(String::length).reversed());
+        for(Map.Entry<String, Set<String>> m : projects.entrySet()){
+            if(m.getValue().contains(developer)){
+                queue.add(m.getKey());
+            }
+        }
+        PriorityQueue<String> queue_2 = new PriorityQueue<>(Comparator.comparing(String::toString).reversed());
+        queue_2.addAll(queue);
+        for(String s : queue){
+            list.add(s);
+        }
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (Map.Entry<String, Integer> m : sourceMap.entrySet()){
+            if(map.containsKey(m.getKey().length())){
+                map.get(m.getKey().length()).add(m.getKey());
+            } else {
+                Set<String> set = new HashSet<>();
+                set.add(m.getKey());
+                map.put(m.getKey().length(), set);
+            }
+
+        }
+        return map;
     }
 }
